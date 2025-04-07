eccdf66e8d8e30ce61a1956862a2df23545ee339
rohitid33 <167289677+rohitid33@users.noreply.github.com>
2025-04-07 23:19:55 +0530

    herobanner

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -28,7 +28,7 @@ const Hero = () => {
 
   return (
     <div 
-      className="hero-section relative w-full min-h-[800px] sm:min-h-[500px] max-w-[100vw] mx-auto"
+      className="hero-section relative w-screen left-[50%] right-[50%] -ml-[50vw] -mr-[50vw] min-h-[800px] sm:min-h-[500px]"
       style={{ 
         backgroundImage: `url(${heroImageUrl})`,
         backgroundSize: 'cover',
@@ -39,7 +39,7 @@ const Hero = () => {
       }}
     >
       <div className="absolute inset-0 bg-black bg-opacity-5 flex items-center justify-center">
-        <div className="px-6 md:px-12 max-w-4xl z-10 w-full">
+        <div className="container mx-auto px-6 md:px-12 max-w-4xl z-10">
           <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold mb-4 text-white leading-tight tracking-tight text-center">
             <span className="block bg-clip-text text-transparent bg-gradient-to-r from-white to-indigo-200">
               Take a Tymo
